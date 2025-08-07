e52a1cf281299b3c21bb0ea4a39c945aa7961279
VinamraBhatnagar <vinibhatnagar123@gmail.com>
2025-08-07 11:15:58 +0530

    [2E] Added file

---
+++ add.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class amber{
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter the value:");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=a+b;
+        System.err.println(c);
+
+    }
+}
+
